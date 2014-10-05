1b29b5fa4ae421c292b3d92eafb032ca78c60dc9
Nicole M <nicmatts@gmail.com>
2014-10-05 10:22:12 -0500

    Create categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,4 @@ class Category < ActiveRecord::Base
 	has_many :article_categories
     has_many :categories, through: :article_categories
     validates :title, presence: true, uniqueness: true
-    validates :content, presence: true
 end
