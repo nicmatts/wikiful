08d2743f5a5d1385acada34d3a6635d7c2200cca
Nicole M <nicmatts@gmail.com>
2014-10-08 19:39:41 -0500

    Create authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,9 @@ class ApplicationController < ActionController::Base
     def get_categories
       @categories = Category.all.order(:title)
     end
+
+    def current_user
+    	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    helper_method :current_user
 end
